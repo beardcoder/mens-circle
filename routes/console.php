d80e90235a029da8afef306aad0301aad16cccd5
Markus Sommer <markus@letsbenow.de>
2025-12-24 22:46:25 +0100

    feat: add daily backup commands to console schedule

--- routes/console.php
+++ routes/console.php
@@ -19,3 +19,6 @@ Schedule::command('events:send-reminders')
     ->onFailure(function () {
         error('Event reminders failed to send');
     });
+
+Schedule::command('backup:clean')->daily()->at('01:00');
+Schedule::command('backup:run')->daily()->at('01:30');
