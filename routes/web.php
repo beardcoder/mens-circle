93ce6433c296a6693bdea3e59b1a6a16ed2da8d8
Markus Sommer <markus@letsbenow.de>
2025-12-27 20:32:35 +0100

    feat: add public testimonial submission form (#58)

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\EventController;
 use App\Http\Controllers\LlmsController;
 use App\Http\Controllers\NewsletterController;
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\TestimonialSubmissionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/llms.txt', [LlmsController::class, 'show'])->name('llms.txt');
@@ -11,12 +12,14 @@ Route::get('/llms.txt', [LlmsController::class, 'show'])->name('llms.txt');
 Route::get('/', [PageController::class, 'home'])->name('home');
 Route::get('/event', [EventController::class, 'showNext'])->name('event.show');
 Route::get('/event/{slug}', [EventController::class, 'show'])->name('event.show.slug');
+Route::get('/teile-deine-erfahrung', [TestimonialSubmissionController::class, 'show'])->name('testimonial.form');
 
 // Non-cached routes
 Route::middleware('doNotCacheResponse')->group(function () {
     Route::post('/event/register', [EventController::class, 'register'])->name('event.register');
     Route::post('/newsletter/subscribe', [NewsletterController::class, 'subscribe'])->name('newsletter.subscribe');
     Route::get('/newsletter/unsubscribe/{token}', [NewsletterController::class, 'unsubscribe'])->name('newsletter.unsubscribe');
+    Route::post('/testimonial/submit', [TestimonialSubmissionController::class, 'submit'])->name('testimonial.submit');
 });
 
 // Dynamic pages (must be last to avoid conflicts)
