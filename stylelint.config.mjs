05ba547098ecccc04022de261461eef66854bcc1
Markus Sommer <markus@letsbenow.de>
2025-12-28 15:54:10 +0100

    feat: add Stylelint configuration with logical properties support

---
+++ stylelint.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('stylelint').Config} */
+export default {
+    plugins: ['stylelint-use-logical'],
+    extends: ['stylelint-config-standard', 'stylelint-config-recess-order'],
+    rules: {
+        'csstools/use-logical': 'always',
+        'selector-class-pattern': null,
+    },
+};
