ff53afb4708e9a6224c3f8f99ac12d414a006acc
Markus Sommer <markus@letsbenow.de>
2025-12-28 16:06:07 +0100

    feat: implement passkeys functionality with migration, user model integration, and frontend scripts

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -20,6 +20,7 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
+use MarcelWeidum\Passkeys\PasskeysPlugin;
 
 class AdminPanelProvider extends PanelProvider
 {
