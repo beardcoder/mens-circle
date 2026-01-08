b5bbd6307416a62da2bbe7d9e104e210d41695d3
Markus Sommer <markus@letsbenow.de>
2026-01-08 16:39:49 +0000

    Fix media migration bug in content blocks - use correct block_id variable

--- app/Filament/Resources/Pages/Pages/CreatePage.php
+++ app/Filament/Resources/Pages/Pages/CreatePage.php
@@ -40,16 +40,14 @@ class CreatePage extends CreateRecord
             ]);
 
             // Migriere Media Library Zuordnungen falls vorhanden
-            if (isset($blockData['data']['block_id'])) {
-                Media::where('model_type', get_class($record))
-                    ->where('model_id', $record->id)
-                    ->where('collection_name', 'page_blocks')
-                    ->where('custom_properties->block_id', $blockData['data']['block_id'])
-                    ->update([
-                        'model_type' => ContentBlock::class,
-                        'model_id' => $contentBlock->id,
-                    ]);
-            }
+            Media::where('model_type', get_class($record))
+                ->where('model_id', $record->id)
+                ->where('collection_name', 'page_blocks')
+                ->where('custom_properties->block_id', $blockId)
+                ->update([
+                    'model_type' => ContentBlock::class,
+                    'model_id' => $contentBlock->id,
+                ]);
         }
 
         return $record;
