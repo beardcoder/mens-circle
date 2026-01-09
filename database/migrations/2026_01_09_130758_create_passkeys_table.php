f76e0e832628ff9c4dc9d4da3d1a2dcf17e221dc
Markus Sommer <markus@letsbenow.de>
2026-01-09 13:29:33 +0100

    feat: add migration files for cache, content blocks, events, and user management tables

---
+++ database/migrations/2026_01_09_130758_create_passkeys_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('passkeys', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('authenticatable_id')->constrained('users', 'id', 'passkeys_authenticatable_fk')->cascadeOnDelete();
+            $table->text('name');
+            $table->text('credential_id');
+            $table->jsonb('data');
+            $table->timestamp('last_used_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('passkeys');
+    }
+};
