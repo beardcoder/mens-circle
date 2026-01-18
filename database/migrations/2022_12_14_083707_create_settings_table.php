d56da63a714a5c0f7f149e5057202b0a72e7b1d6
Markus Sommer <markus@letsbenow.de>
2026-01-18 21:25:18 +0100

    refactor: rename migration files and update cache table structure

--- database/migrations/2022_12_14_083707_create_settings_table.php
+++ database/migrations/2022_12_14_083707_create_settings_table.php
@@ -1,34 +1,24 @@
 <?php
 
-declare(strict_types=1);
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class () extends Migration {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+return new class extends Migration
+{
+    public function up()
     {
         Schema::create('settings', function (Blueprint $table): void {
             $table->id();
+
             $table->string('group');
             $table->string('name');
             $table->boolean('locked')->default(false);
-            $table->jsonb('payload');
+            $table->json('payload');
+
             $table->timestamps();
 
             $table->unique(['group', 'name']);
         });
-    }
-
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('settings');
     }
 };
