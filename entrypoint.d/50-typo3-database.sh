5496efd5f61a18af24d853a6afef6c86718b1261
Markus <markus@letsbenow.de>
2026-02-16 15:09:24 +0100

    50-typo3-database.sh aktualisieren

--- entrypoint.d/50-typo3-database.sh
+++ entrypoint.d/50-typo3-database.sh
@@ -6,8 +6,5 @@ set -e
 
 TYPO3="${APP_BASE_DIR:-/var/www/html}/vendor/bin/typo3"
 
-echo "TYPO3: Updating database schema..."
-"$TYPO3" database:updateschema "safe"
-
 echo "TYPO3: Setting up extensions..."
 "$TYPO3" extension:setup
