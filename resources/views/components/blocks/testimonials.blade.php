5ecfa6585f279d746d8bde52a7ee89d851af2951
Markus Sommer <markus@letsbenow.de>
2026-02-24 14:09:09 +0100

    Convert testimonials to divider layout and fix header centering

--- resources/views/components/blocks/testimonials.blade.php
+++ resources/views/components/blocks/testimonials.blade.php
@@ -39,28 +39,22 @@
 
         <div class="testimonials__grid stagger-children">
             @foreach($testimonials as $testimonial)
-                <div class="testimonial-card">
-                    <div class="testimonial-card__quote-icon">
-                        <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M10 16C10 13.7909 8.20914 12 6 12V8C10.4183 8 14 11.5817 14 16V22C14 23.1046 13.1046 24 12 24H8C6.89543 24 6 23.1046 6 22V18C6 16.8954 6.89543 16 8 16H10ZM24 16C24 13.7909 22.2091 12 20 12V8C24.4183 8 28 11.5817 28 16V22C28 23.1046 27.1046 24 26 24H22C20.8954 24 20 23.1046 20 22V18C20 16.8954 20.8954 16 22 16H24Z" fill="currentColor"/>
-                        </svg>
-                    </div>
-
-                    <blockquote class="testimonial-card__quote">
+                <article class="testimonial-item">
+                    <blockquote class="testimonial-item__quote">
                         {{ $testimonial->quote }}
                     </blockquote>
 
                     @if($testimonial->author_name || $testimonial->role)
-                        <div class="testimonial-card__author">
+                        <div class="testimonial-item__author">
                             @if($testimonial->author_name)
-                                <cite class="testimonial-card__name">{{ $testimonial->author_name }}</cite>
+                                <cite class="testimonial-item__name">{{ $testimonial->author_name }}</cite>
                             @endif
                             @if($testimonial->role)
-                                <span class="testimonial-card__role">{{ $testimonial->role }}</span>
+                                <span class="testimonial-item__role">{{ $testimonial->role }}</span>
                             @endif
                         </div>
                     @endif
-                </div>
+                </article>
             @endforeach
         </div>
     </div>
