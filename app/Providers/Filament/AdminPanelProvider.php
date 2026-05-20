1e6cbf3d1aea2ff0f9a75cbea21ce346673e88a7
Markus Sommer <markus@letsbenow.de>
2026-05-20 15:06:09 +0200

    feat(auth): implement custom login page with default user credentials for local development

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Providers\Filament;
 
+use App\Filament\Pages\Auth\Login;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
@@ -32,7 +33,7 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
-            ->login()
+            ->login(Login::class)
             ->spa()
             ->databaseTransactions()
             ->sidebarCollapsibleOnDesktop()
